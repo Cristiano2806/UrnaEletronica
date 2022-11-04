fcf7e404e8516a359d51f503bb531a078da2f5d7
Cristiano <cristiano.botelho@sou.unifeob.edu.br>
2022-11-04 04:28:39 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo : 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'zetao.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'rafael.png', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo : 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '45',
+                nome: 'Zetinho',
+                partido: 'PSDB',
+                vice: 'Rafael',
+                fotos:[
+                    {url:'zetao.jpg', legenda: 'Prefeito'},
+                    {url:'rafael.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Rafael',
+                partido: 'PL',
+                vice: 'Zetinho',
+                fotos:[
+                    {url:'rafael.png', legenda: 'Prefeito'},
+                    {url:'zetao.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
